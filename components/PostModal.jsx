bb0dba08124d1d030fbfc62d4d848ed46339e9ce
abdinasir-Tman <abdinasirmursal2@gmail.com>
2023-09-06 07:20:08 -0700

    modified

--- components/PostModal.jsx
+++ components/PostModal.jsx
@@ -60,7 +60,7 @@ const PostModal = ({ fetchPosts }) => {
         onRequestClose={closeModal}
         // style={customStyles}
         contentLabel="Example Modal"
-        className="shadow-lg fixed bg-white p-4 rounded-md top-0 left-0 right-0 md:right-10 md:left-10 md:top-10 lg:left-20 lg:right-20 lg:top-20 bottom-20"
+        className="shadow-lg fixed bg-white p-4 rounded-md top-0  left-0 right-0 md:right-12 md:left-12 md:top-12 lg:left-20 lg:right-20 lg:top-20 lg:bottom-20 bottom-0"
       >
         {/* <h2
           ref={(_subtitle) => (subtitle = _subtitle)}
